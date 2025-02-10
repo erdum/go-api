aa9d4fa62d5fc765e13d4578a63b2bee0412757a
erdum <erdumadnan@gmail.com>
2025-02-10 12:47:39 +0500

    Fix: Lenght validation corrected

--- requests/register_request.go
+++ requests/register_request.go
@@ -3,6 +3,6 @@ package requests
 type RegisterRequest struct {
 	Name			string `json:"name" validate:"required"`
 	Email			string `json:"email" validate:"required,email"`
-	PhoneNumber		string `json:"phoneNumber" validate:"required,lte=15"`
-	Password		string `json:"password" validate:"required,gte=8"`
+	PhoneNumber		string `json:"phoneNumber" validate:"required,max=15"`
+	Password		string `json:"password" validate:"required,min=8"`
 }
